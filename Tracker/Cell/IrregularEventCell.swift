70d499784e4f8ee0842fa951e8855fd5c86624de
Xiered <vnyantost@gmail.com>
2024-05-01 19:30:39 +0400

    Added IrregularEventCell

--- Tracker/Cell/IrregularEventCell.swift
+++ Tracker/Cell/IrregularEventCell.swift
@@ -5,4 +5,43 @@
 //  Created by Дмитрий Герасимов on 30.04.2024.
 //
 
-import Foundation
+import UIKit
+
+final class IrregularEventCell: UITableViewCell {
+    
+    static let reuseIdentifier = "IrregularEventCell"
+    
+    private let arrowImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "chevron.right")
+        imageView.tintColor = UIColor(named: "YP Gray")
+        
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupCell()
+    }
+    
+    func setupCell() {
+        textLabel?.font = UIFont.systemFont(ofSize: 17)
+        textLabel?.textColor = UIColor(named: "YP Black (day)")
+        detailTextLabel?.textColor = UIColor(named: "YP Gray")
+        
+        layoutMargins = .zero
+        separatorInset = .zero
+        contentView.addSubview(arrowImageView)
+        
+        NSLayoutConstraint.activate([
+            arrowImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            arrowImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
