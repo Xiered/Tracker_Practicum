9b88216be4972acd96471613c7c94adeb52750eb
Xiered <vnyantost@gmail.com>
2024-05-01 19:24:43 +0400

    Layout Header reusable

--- Tracker/View/HeaderCollectionReusableView.swift
+++ Tracker/View/HeaderCollectionReusableView.swift
@@ -5,4 +5,28 @@
 //  Created by Дмитрий Герасимов on 30.04.2024.
 //
 
-import Foundation
+import UIKit
+
+final class HeaderCollectionReusableView: UICollectionReusableView {
+    
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(named: "YP Black (day)")
+        label.font = .boldSystemFont(ofSize: 19)
+        
+        return label
+    }()
+    
+    func configureHeader(text: String) {
+        headerLabel.text = text
+        
+        addSubview(headerLabel)
+        
+        NSLayoutConstraint.activate([
+            headerLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            headerLabel.topAnchor.constraint(equalTo: topAnchor),
+            headerLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+        ])
+    }
+}
