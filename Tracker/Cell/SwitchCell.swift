cad12c2ba358126fdc3e707487dd23b38cb50393
Xiered <vnyantost@gmail.com>
2024-05-03 13:02:13 +0400

    Add SwitchCell and fix category cell

--- Tracker/Cell/SwitchCell.swift
+++ Tracker/Cell/SwitchCell.swift
@@ -5,4 +5,50 @@
 //  Created by Дмитрий Герасимов on 30.04.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol SwitchCellDelegate: AnyObject {
+    func switchCellDidToggle(_ cell: SwitchCell, isOn: Bool)
+}
+
+final class SwitchCell: UITableViewCell {
+    
+    static let reuseIdentifier = "SwitchCell"
+    weak  var delegate: SwitchCellDelegate?
+    
+    let switcher: UISwitch = {
+        let switcher = UISwitch()
+        switcher.translatesAutoresizingMaskIntoConstraints = false
+        switcher.tintColor = UIColor(named: "YP White (day)")
+        switcher.onTintColor = UIColor(named: "YP Blue")
+        
+        return switcher
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupLayout()
+        selectionStyle = .none
+        
+        switcher.addTarget(self, action: #selector(switcherValueChanged), for: .valueChanged)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setupLayout() {
+        textLabel?.font = UIFont.systemFont(ofSize: 17)
+        textLabel?.textColor = UIColor(named: "YP Black (day)")
+        contentView.addSubview(switcher)
+        
+        NSLayoutConstraint.activate([
+            switcher.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            switcher.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    @objc private func switcherValueChanged(_ sender: UISwitch) {
+        delegate?.switchCellDidToggle(self, isOn: sender.isOn)
+    }
+}
--- Tracker/Cell/TrackerViewCell.swift
+++ Tracker/Cell/TrackerViewCell.swift
@@ -7,25 +7,6 @@
 
 import UIKit
 
-final class CategoryCell: UITableViewCell {
+final class TrackerViewCell: UITableViewCell {
     
-    static let reuseIdentifier = "CategoryCell"
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupCell()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupCell()
-    }
-
-    func setupCell() {
-        textLabel?.font = UIFont.systemFont(ofSize: 17)
-        textLabel?.textColor = UIColor(named: "YP Black (day)")
-
-        layoutMargins = .zero
-        separatorInset = .zero
-    }
 }
