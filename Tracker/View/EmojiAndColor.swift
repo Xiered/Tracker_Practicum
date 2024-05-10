5b41f2829925a78b6ce7d70f9ad9d6ae99cb887f
Xiered <vnyantost@gmail.com>
2024-05-10 16:18:42 +0400

    Preparing to display emoji and color selection

---
+++ Tracker/View/EmojiAndColor.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAndColor.swift
+//  Tracker
+//
+//  Created by Дмитрий Герасимов on 10.05.2024.
+//
+
+import UIKit
+
+protocol EmojiAndColorDelegate: AnyObject {
+    func emojiAndColorDidSelectEmoji(_ emoji: String)
+    func emojiAndColorDidSelectColor(_ color: UIColor)
+}
+
+final class EmojiAndColor {
+    
+}
